aec221ac6dba7851929325a873e12cd62dc12b35
vagrant <vagrant@precise32.(none)>
2016-01-10 17:57:01 +0000

    basic readme file

--- controllers/main.py
+++
@@ -1,8 +0,0 @@
-from flask import *
-from extensions import mysql
-
-main = Blueprint('main', __name__, template_folder='templates')
-
-@main.route('/')
-def main_route():
-    return render_template("index.html")
